efb4eac4e5c77138f4693c684b92fdd3510f228c
Jag <72930861+jagman014@users.noreply.github.com>
2022-04-13 20:36:44 +0100

    Completed Chapter 3 of WebDev with Django book

--- WebDevWithDjangoBook/bookr/reviews/views.py
+++ WebDevWithDjangoBook/bookr/reviews/views.py
@@ -1,11 +1,56 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Book, Review
+from .utils import average_rating
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'base.html')
+def book_list(request: HttpRequest) -> HttpResponse:
+    books = Book.objects.all()
+    book_list = []
+
+    for book in books:
+        reviews = book.review_set.all()
+
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        
+        book_list.append(
+            {
+                'book': book,
+                'book_rating': book_rating,
+                'number_of_reviews': number_of_reviews
+            }
+        )
+    
+    context = {'book_list': book_list}
+
+    return render(request, 'reviews/book_list.html', context)
 
 
-def search(request: HttpRequest) -> HttpResponse:
-    search_input = request.GET.get('search')
-    return render(request, 'search.html', {'search_input': search_input})
+def book_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+
+        context = {
+            'book': book,
+            'book_rating': book_rating,
+            'reviews': reviews
+        }
+    
+    else:
+        context = {
+            'book': book,
+            'book_rating': None,
+            'reviews': None
+        }
+    
+    return render(request, 'reviews/book_detail.html', context)
